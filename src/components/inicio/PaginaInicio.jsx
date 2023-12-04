64435803181029f238a763d45c775fe3a1b9e1e8
Juan Carlos <jucasaguipn09@gmail.com>
2023-12-03 20:26:18 -0600

    Main Page

--- src/components/inicio/PaginaInicio.jsx
+++ src/components/inicio/PaginaInicio.jsx
@@ -1,9 +1,130 @@
 import { Link } from "react-router-dom";
-import { ChevronRightIcon } from "@heroicons/react/20/solid";
+import fondo from "../../assets/a.svg";
+import img1 from "../../assets/citas.png";
+import img2 from "../../assets/clinica.png";
+import img3 from "../../assets/configuraciones.png";
 import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Typography } from "@material-tailwind/react";
+import {
+  ArrowPathIcon,
+  CloudArrowUpIcon,
+  FingerPrintIcon,
+  LockClosedIcon,
+} from "@heroicons/react/24/outline";
+
+const features = [
+  {
+    name: "Pacientes",
+    description:
+      "Agrega, edita y elimina pacientes de tu clinica, ademas de poder ver su historial clinico y sus recetas medicas.",
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: "Citas y Recetas",
+    description:
+      "Agenda citas con tus pacientes y genera recetas medicas para ellos.",
+    icon: LockClosedIcon,
+  },
+  {
+    name: "Notificaciones",
+    description:
+      "Recibe notificaciones de tus citas medicas 2 horas antes de que se lleven a cabo.",
+    icon: ArrowPathIcon,
+  },
+  {
+    name: "Doctores",
+    description:
+      "Agrega, edita y elimina doctores de tu clinica, añade sus especialidades y sus horarios de trabajo.",
+    icon: FingerPrintIcon,
+  },
+];
+
+const navigation = [
+  {
+    name: "Facebook",
+    href: "#",
+    icon: (props) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path
+          fillRule="evenodd"
+          d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: "Instagram",
+    href: "#",
+    icon: (props) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path
+          fillRule="evenodd"
+          d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: "Twitter",
+    href: "#",
+    icon: (props) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+      </svg>
+    ),
+  },
+  {
+    name: "GitHub",
+    href: "#",
+    icon: (props) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path
+          fillRule="evenodd"
+          d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: "YouTube",
+    href: "#",
+    icon: (props) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path
+          fillRule="evenodd"
+          d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+];
+
+const posts = [
+  {
+    title: "Calendario dinámico",
+    description:
+      " SGCD cuenta con un calendario dinámico que te permite visualizar tus citas de manera rápida y sencilla.",
+    imageUrl: img1,
+  },
+  {
+    title: "Detalles de la clinica",
+    description:
+      "Agrega, edita y elimina los detalles de tu clinica, como el nombre, la direccion, el telefono asi tus pacientes podran contactarte.",
+    imageUrl: img2,
+  },
+  {
+    title: "Configuraciones",
+    description:
+      "Agrega configuraciones personalizadas para tus doctores, como los horarios y dias de trabajo.",
+    imageUrl: img3,
+  },
+];
 
 export default function PaginaInicio() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -13,11 +134,11 @@ export default function PaginaInicio() {
       <div>
         <header className="absolute inset-x-0 top-0 z-50">
           <nav
-            className="flex items-center justify-between p-6 lg:px-8"
+            className="flex items-center justify-between p-6 lg:px-16 2xl:py-24"
             aria-label="Global"
           >
             <div className="flex lg:flex-1">
-              <a href="#" className="-m-1.5 p-1.5">
+              <Link to="/" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
                 <Typography
                   variant="h3"
@@ -27,12 +148,12 @@ export default function PaginaInicio() {
                 >
                   SGCD
                 </Typography>
-              </a>
+              </Link>
             </div>
             <div className="flex lg:hidden">
               <button
                 type="button"
-                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
+                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
                 onClick={() => setMobileMenuOpen(true)}
               >
                 <span className="sr-only">Open main menu</span>
@@ -62,9 +183,9 @@ export default function PaginaInicio() {
             onClose={setMobileMenuOpen}
           >
             <div className="fixed inset-0 z-50" />
-            <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-blue-500 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
+            <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-azure-500 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
               <div className="flex items-center justify-between">
-                <a href="#" className="-m-1.5 p-1.5">
+                <Link to="/login" className="-m-1.5 p-1.5">
                   <span className="sr-only">Your Company</span>
                   <Typography
                     variant="h3"
@@ -74,10 +195,10 @@ export default function PaginaInicio() {
                   >
                     SGCD
                   </Typography>
-                </a>
+                </Link>
                 <button
                   type="button"
-                  className="-m-2.5 rounded-md p-2.5 text-gray-400"
+                  className="-m-2.5 rounded-md p-2.5 text-white"
                   onClick={() => setMobileMenuOpen(false)}
                 >
                   <span className="sr-only">Close menu</span>
@@ -87,12 +208,12 @@ export default function PaginaInicio() {
               <div className="mt-6 flow-root">
                 <div className="-my-6 divide-y divide-gray-500/25">
                   <div className="py-6">
-                    <a
-                      href="#"
+                    <Link
+                      to="/login"
                       className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white"
                     >
                       Iniciar Sesion
-                    </a>
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -100,13 +221,11 @@ export default function PaginaInicio() {
           </Dialog>
         </header>
 
-        <div className="relative isolate overflow-hidden pt-14 h-[calc(100vh)] ">
-          <img
-            src="https://cdn.pixabay.com/photo/2014/11/27/20/12/doctor-548133_1280.jpg"
-            alt=""
-            className="absolute inset-0 -z-10 h-full w-full object-cover"
-          />
-          <div className="absolute inset-0 bg-blue-400/70"></div>
+        <div className="relative isolate overflow-hidden h-[calc(100vh)] mx-0">
+          <svg className="w-full h-full">
+            <image href={fondo} className="md:w-full h-full" />
+          </svg>
+          <div className="absolute inset-0"></div>
           <div className="absolute inset-0 flex justify-center items-center">
             <div
               className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
@@ -122,34 +241,22 @@ export default function PaginaInicio() {
             </div>
             <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
               <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
-              <div className="text-center bg-blue-200/70 py-10 rounded-3xl">
+              <div className="text-center">
                 <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
                   Sistema de Gestion Clinica para Doctores
                 </h1>
-                <Typography variant="h5" className="mt-5 text-gray-900">
+                <Typography variant="h5" className="mt-5" color="white">
                   Gestione su clinica de manera facil y rapida, con un sistema
                   que le permitira gestionar sus pacientes, citas, medicamentos,
                   recetas y mas.
                 </Typography>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
-                  <a
-                    href="#"
-                    className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white"
+                  <Link
+                    to="/signup"
+                    className="w-64 rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-azure-500"
                   >
                     Comenzar
-                  </a>
-                  <a
-                    href="#"
-                    className="text-sm font-semibold leading-6 text-white"
-                  >
-                    <Typography
-                      variant="h6"
-                      fontWeight="bold"
-                      className="text-gray-900"
-                    >
-                      Inciar Sesion <span aria-hidden="true">&rarr;</span>
-                    </Typography>
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
@@ -168,6 +275,152 @@ export default function PaginaInicio() {
           </div>
         </div>
       </div>
+
+      <div className="bg-white py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:text-center">
+            <h2 className="text-base font-semibold leading-7 text-indigo-600"></h2>
+            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Todo lo que necesitas para gestionar tu clinica
+            </p>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Todas las herramientas que necesitas para gestionar tu clinica de
+              manera facil y rapida, con un sistema que le permitira gestionar
+              sus pacientes, citas, medicamentos, recetas y mas.
+            </p>
+          </div>
+          <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+            <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+              {features.map((feature) => (
+                <div key={feature.name} className="relative pl-16">
+                  <dt className="text-base font-semibold leading-7 text-gray-900">
+                    <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                      <feature.icon
+                        className="h-6 w-6 text-white"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    {feature.name}
+                  </dt>
+                  <dd className="mt-2 text-base leading-7 text-gray-600">
+                    {feature.description}
+                  </dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+        </div>
+      </div>
+
+      <div className="relative isolate overflow-hidden bg-azure-500 py-24 sm:py-32">
+        <div
+          className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+          aria-hidden="true"
+        >
+          <div
+            className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+        <div
+          className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+          aria-hidden="true"
+        >
+          <div
+            className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto  lg:mx-0">
+            <div className="flex justify-between gap-10">
+              <div>
+                <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                  Comienza a usar SGCD hoy mismo
+                </h2>
+                <p className="mt-6 text-lg leading-8 text-gray-300">
+                  El sistema de gestion clinica para doctores de profesion libre
+                  que necesitas para gestionar tu clinica de manera facil y
+                  rapida.
+                </p>
+              </div>
+              <img
+                src="https://cdn.pixabay.com/photo/2014/11/27/20/12/doctor-548133_1280.jpg"
+                alt=""
+                className="w-[500px] object-cover rounded-xl shadow-xl "
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl text-center">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Por que elegir SGCD
+            </h2>
+            <p className="mt-2 text-lg leading-8 text-gray-600">
+              A continuacion te mostramos algunas de las razones por las que
+              deberias elegir SGCD para gestionar tu clinica.
+            </p>
+          </div>
+          <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            {posts.map((post) => (
+              <article
+                key={post.id}
+                className="flex flex-col items-start justify-between"
+              >
+                <div className="relative w-full">
+                  <img
+                    src={post.imageUrl}
+                    alt=""
+                    className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+                  />
+                  <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+                </div>
+                <div className="max-w-xl">
+                  <div className="group relative">
+                    <h3 className="mt-12 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                      <a href={post.href}>
+                        <span className="absolute inset-0" />
+                        {post.title}
+                      </a>
+                    </h3>
+                    <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                      {post.description}
+                    </p>
+                  </div>
+                </div>
+              </article>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <footer className="bg-azure-500 text-white">
+        <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
+          <div className="flex justify-center space-x-6 md:order-2">
+            {navigation.map((item) => (
+              <a key={item.name} href={item.href} className="">
+                <span className="sr-only">{item.name}</span>
+                <item.icon className="h-6 w-6" aria-hidden="true" />
+              </a>
+            ))}
+          </div>
+          <div className="mt-8 md:order-1 md:mt-0">
+            <p className="text-center text-xs leading-5">
+              &copy; 2023 SGCD, Inc. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
